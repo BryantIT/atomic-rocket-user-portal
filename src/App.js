6ca77bc7717a27e1c8574a68a656a8a16b5b1995
Bryant Richards <dragonseyeimages@gmail.com>
2022-08-14 09:41:08 -0500

    site-header style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // Components
-import Header from './components/Header'
+import Header from './components/header/Header'
 import './App.css';
 
 function App() {
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,6 @@
 import React from 'react'
+//stylesheet
+import './style.css'
 
 const Header = () => {
     return (
@@ -7,62 +9,62 @@ const Header = () => {
 			<input type="search" name="search" placeholder="Type and Hit Enter.." />
 			<button>Search</button>
 		</form>
-		<div class="container nav-bar-container">
-			<div class="row nav-row">
-				<div class="logo-container">
+		<div className="container nav-bar-container">
+			<div className="row nav-row">
+				<div className="logo-container">
 					<a href="index.html">
-						<span class="logo ">Music<span class="colored">Base</span></span>
+						<span className="logo ">Music<span className="colored">Base</span></span>
 						<br />
-						<span class="tagline colored-orange">Electro Music Base</span>
+						<span className="tagline colored-orange">Electro Music Base</span>
 					</a>
 				</div>
-				<div id="site-navigation" class="tech-nav">
+				<div id="site-navigation" className="tech-nav">
 					<ul>
-						<li class="active has-child">
+						<li className="active has-child">
 							<a href="#">Home</a>
-							<ul class="submenu">
+							<ul className="submenu">
 								<li><a href="index.html">Home Band</a></li>
 								<li><a href="home-artist.html">Home Artist</a></li>
 								<li><a href="home-podcast.html">Home Podcast</a></li>
 							</ul>
 						</li>
-						<li class="has-child">
+						<li className="has-child">
 							<a href="#">Albums</a>
-							<ul class="submenu">
+							<ul className="submenu">
 								<li><a href="albums.html">Albums Page</a></li>
 								<li><a href="album-single.html">Album Single</a></li>
 							</ul>
 						</li>
-						<li class="has-child">
+						<li className="has-child">
 							<a href="#">Events</a>
-							<ul class="submenu">
+							<ul className="submenu">
 								<li><a href="events-masonry.html">Events Masonry</a></li>
 								<li><a href="events-list.html">Events List</a></li>
 								<li><a href="event-single.html">Event Single</a></li>
 							</ul>
 						</li>
-						<li class="has-child">
+						<li className="has-child">
 							<a href="#">Band</a>
-							<ul class="submenu">
+							<ul className="submenu">
 								<li><a href="members.html">Members Page</a></li>
 								<li><a href="biography.html">Biography Page</a></li>
 								<li><a href="artist-single.html">Single Artist</a></li>
 							</ul>
 						</li>
-						<li class="has-child">
+						<li className="has-child">
 							<a href="#">Pages</a>
-							<ul class="submenu">
-								<li class="has-child">
+							<ul className="submenu">
+								<li className="has-child">
 									<a href="#">Blog</a>
-									<ul class="submenu">
+									<ul className="submenu">
 										<li><a href="blog-list.html">Blog List</a></li>
 										<li><a href="blog-masonry.html">Blog Masonry</a></li>
 										<li><a href="blog-single.html">Blog Single</a></li>
 									</ul>
 								</li>
-								<li class="has-child">
+								<li className="has-child">
 									<a href="#">Shop</a>
-									<ul class="submenu">
+									<ul className="submenu">
 										<li><a href="products.html">Products</a></li>
 										<li><a href="product-single.html">Product Single</a></li>
 										<li><a href="cart.html">Cart</a></li>
@@ -77,29 +79,29 @@ const Header = () => {
 					</ul>
 				</div>
 				{/* <!-- Extra Nav --> */}
-				<div class="extra-nav">
-					<button id="mini-cart-toggle"><i class="fa fa-shopping-cart"></i> <span class="mini-cart-count">2</span></button>
-					<button id="search-toggle"><i class="fa fa-search"></i></button>
-					<button id="menu-toggle"><i class="fa fa-bars"></i></button>
+				<div className="extra-nav">
+					<button id="mini-cart-toggle"><i className="fa fa-shopping-cart"></i> <span className="mini-cart-count">2</span></button>
+					<button id="search-toggle"><i className="fa fa-search"></i></button>
+					<button id="menu-toggle"><i className="fa fa-bars"></i></button>
 					{/* <!-- Mini Cart Widget --> */}
 					<div id="mini-cart-widget">
-						<h5 class="title">2 Products</h5>
-						<div class="product clearfix">
+						<h5 className="title">2 Products</h5>
+						<div className="product clearfix">
 							<img src="assets/img/shop/mini-1.jpg" alt="" />
 							<h6>This is Product Title</h6>
 							<span>1 x $120</span>
-							<i class="fa fa-close"></i>
+							<i className="fa fa-close"></i>
 						</div>
-						<div class="product clearfix">
+						<div className="product clearfix">
 							<img src="assets/img/shop/mini-2.jpg" alt="" />
 							<h6>This is Product Title</h6>
 							<span>2 x $60</span>
-							<i class="fa fa-close"></i>
+							<i className="fa fa-close"></i>
 						</div>
-						<div class="mini-cart-footer">
-							<h4 class="subtotal">Subtotal: <span>$240</span></h4>
-							<a href="#" class="btn btn-sm">Checkout</a >
-							<a href="#" class="link">View Cart</a>
+						<div className="mini-cart-footer">
+							<h4 className="subtotal">Subtotal: <span>$240</span></h4>
+							<a href="#" className="btn btn-sm">Checkout</a >
+							<a href="#" className="link">View Cart</a>
 						</div>
 					</div>
 				</div>
