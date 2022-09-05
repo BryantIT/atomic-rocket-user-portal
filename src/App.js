0fb00e99be2cd0878bee890ed8cb67dd42e927b8
Bryant Richards <dragonseyeimages@gmail.com>
2022-09-05 09:01:49 -0500

    404 component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Fragment } from 'react'
 import Header from './components/header/Header'
 import Home from './pages/Home'
 import Footer from './components/footer/Footer'
+import FourOhFour from './components/errors/FourOhFour'
 // Router
 import { Routes, Route } from 'react-router-dom'
 // Styles
@@ -18,6 +19,7 @@ function App() {
         </Routes>
       </div>
       <Footer />
+      <FourOhFour />
     </Fragment>
   )
 }
